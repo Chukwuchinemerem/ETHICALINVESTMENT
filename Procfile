fa6a540ad64259c57973004d4f1355f10365595e
chukwu prince <chukwuchinemerem258@gmail.com>
2026-06-27 22:25:17 +0100

    update render deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python -m gunicorn profitLynx.wsgi:application --bind 0.0.0.0:$PORT --workers 2
+web: python3 -m gunicorn profitLynx.wsgi:application --bind 0.0.0.0:$PORT --workers 2
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
